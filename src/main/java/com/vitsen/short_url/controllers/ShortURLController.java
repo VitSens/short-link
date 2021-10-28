7af2e4edc394d690cd45712a3d457c01c35ea912
Vitalii <55030208+VitSens@users.noreply.github.com>
2021-10-28 21:04:58 +0300

    Rename variable shortURLService to shortUrlService

--- src/main/java/com/vitsen/short_url/controllers/ShortURLController.java
+++ src/main/java/com/vitsen/short_url/controllers/ShortURLController.java
@@ -1,21 +1,22 @@
 package com.vitsen.short_url.controllers;
 
+import com.vitsen.short_url.models.URL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/")
 public class ShortURLController {
-    private ShortURLService shortURLService;
+    private ShortURLService shortUrlService;
 
     @Autowired
-    public ShortURLController(ShortURLService shortURLService) {
-        this.shortURLService = shortURLService;
+    public ShortURLController(ShortURLService shortUrlService) {
+        this.shortUrlService = shortUrlService;
     }
 
 
     @PostMapping()
     public URL createShortURL(@RequestParam String fullUrl) {
-        return shortURLService.createShortURL(fullUrl);
+        return shortUrlService.createShortURL(fullUrl);
     }
 }
