19ed6b3701a063a1f16ea3e386b8d089de7813e9
Vitalii <55030208+VitSens@users.noreply.github.com>
2021-11-06 21:09:07 +0300

    Edit main class
    Added EnableJpaRepositories above the class

--- src/main/java/com/vitsen/short_url/ShortUrlApplication.java
+++ src/main/java/com/vitsen/short_url/ShortUrlApplication.java
@@ -2,8 +2,10 @@ package com.vitsen.short_url;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.vitsen.short_url.repo")
 public class ShortUrlApplication {
 
     public static void main(String[] args) {
