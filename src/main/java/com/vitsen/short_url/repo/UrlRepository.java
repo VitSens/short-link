0289688a33a6e719bea713574eddfbc9cb9dea63
Vitalii <55030208+VitSens@users.noreply.github.com>
2021-11-07 14:48:14 +0300

    Edit UrlRepository
    Added two custom queries

--- src/main/java/com/vitsen/short_url/repo/UrlRepository.java
+++ src/main/java/com/vitsen/short_url/repo/UrlRepository.java
@@ -2,8 +2,17 @@ package com.vitsen.short_url.repo;
 
 import com.vitsen.short_url.models.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UrlRepository extends JpaRepository<Url, Long> {
+    @Query("FROM Url WHERE fullUrl = :url")
+    List<Url> findAllByUrl(@Param("url") String url);
+
+    @Query(value = "SELECT fullUrl FROM Url WHERE shortUrl = :shortUrl")
+    String findByShortUrl(@Param("shortUrl") String shortUrl);
 }
