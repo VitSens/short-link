425326da6c545606b719c01de831356ccf7bd043
Vitalii <55030208+VitSens@users.noreply.github.com>
2021-11-07 10:25:33 +0300

    Edit ShortUrlController
    Changes:
    - Edited RequestParam to RequestBody in PostMapping
    - Edited margins

--- src/main/java/com/vitsen/short_url/controllers/ShortUrlController.java
+++ src/main/java/com/vitsen/short_url/controllers/ShortUrlController.java
@@ -3,20 +3,17 @@ package com.vitsen.short_url.controllers;
 import com.vitsen.short_url.models.Url;
 import com.vitsen.short_url.services.ShortUrlService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/shortlink")
 @RequiredArgsConstructor
 public class ShortUrlController {
+
     private final ShortUrlService shortUrlService;
 
-
     @PostMapping
-    public Url createShortUrl(@RequestParam String url) {
+    public Url createShortUrl(@RequestBody String url) {
         return shortUrlService.createShortURL(url);
     }
 }
