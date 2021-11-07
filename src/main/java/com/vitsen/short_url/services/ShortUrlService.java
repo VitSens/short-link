2a21649b5bcdccc394ba78bdb1d3e13a6c55c350
Vitalii <55030208+VitSens@users.noreply.github.com>
2021-11-07 14:49:53 +0300

    Edit ShortUrlController
    - Made new 2 methods for GetMapping
    - Made method for generation unique value for shortlink

--- src/main/java/com/vitsen/short_url/services/ShortUrlService.java
+++ src/main/java/com/vitsen/short_url/services/ShortUrlService.java
@@ -9,19 +9,39 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class ShortUrlService {
     private final UrlRepository repository;
 
+    public List<Url> findAllByUrl(String url) {
+        return repository.findAllByUrl(url);
+    }
+
+    public String findByShortUrl(String fullUrl) {
+        return repository.findByShortUrl(fullUrl);
+    }
+
     @Transactional(rollbackFor = Exception.class)
     public String createShortURL(String fullUrl) {
+        String uniqueValueShortUrl = createUniqueValueShortUrl();
         boolean b = UrlMatcherUtility.checkUrlMatch(fullUrl);
         if (!b) throw new NotRightUrlException();
 
-        Url save = repository.save(new Url(fullUrl, "w4324324"));
+        Url save = repository.save(new Url(fullUrl, uniqueValueShortUrl));
 
         return save.getShortUrl();
+    }
+
+    private String createUniqueValueShortUrl() {
+        String rand = UUID.randomUUID().toString();
+
+        String[] a = rand.split("-");
+
+        return a[0];
     }
 }
