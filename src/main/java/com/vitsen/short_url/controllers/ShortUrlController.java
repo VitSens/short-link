3b21856795b6a46bdb98e24997d62bfb31669b33
Vitalii <55030208+VitSens@users.noreply.github.com>
2021-11-07 13:48:44 +0300

    Edit ShortUrlController

--- src/main/java/com/vitsen/short_url/controllers/ShortUrlController.java
+++ src/main/java/com/vitsen/short_url/controllers/ShortUrlController.java
@@ -1,9 +1,12 @@
 package com.vitsen.short_url.controllers;
 
-import com.vitsen.short_url.models.Url;
 import com.vitsen.short_url.services.ShortUrlService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/shortlink")
@@ -13,7 +16,7 @@ public class ShortUrlController {
     private final ShortUrlService shortUrlService;
 
     @PostMapping
-    public Url createShortUrl(@RequestBody String url) {
-        return shortUrlService.createShortURL(url);
+    public ResponseEntity<String> createShortUrl(@RequestBody String url) {
+        return ResponseEntity.ok(shortUrlService.createShortURL(url));
     }
 }
