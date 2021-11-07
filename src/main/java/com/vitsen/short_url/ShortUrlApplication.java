9ab1444c060aaf0e29c6157c52ed3ff96169b233
Vitalii <55030208+VitSens@users.noreply.github.com>
2021-11-07 13:47:52 +0300

    Edit main class
    Deleted EnableJpaRepository annotation

--- src/main/java/com/vitsen/short_url/ShortUrlApplication.java
+++ src/main/java/com/vitsen/short_url/ShortUrlApplication.java
@@ -2,10 +2,8 @@ package com.vitsen.short_url;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.vitsen.short_url.repo")
 public class ShortUrlApplication {
 
     public static void main(String[] args) {
